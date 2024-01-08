126bb1e0166d68957bdbc4a371f0772854d70eeb
Lars Skifjeld <larssski@gmail.com>
2024-01-08 15:35:56 +0100

    read csv algo in rust

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,3 +1,22 @@
+use std::{error::Error, fs::File};
+
+fn read_csv() -> Result<(), Box<dyn Error>> {
+    let file = File::open("./sample.csv");
+    match file {
+        Ok(reader) => {
+            let mut rdr = csv::Reader::from_reader(reader);
+            for result in rdr.records() {
+                let record = result?;
+                println!("{:?}", record);
+            }
+        }
+        Err(err) => println!("Error reading file, {err}"),
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_csv() {
+        println!("error urnning read_csv {err}");
+    }
 }
