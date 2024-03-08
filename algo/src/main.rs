70c8e292356fee034b2e84b2ab01ecf053abd2b8
Lars Skifjeld <larssski@gmail.com>
2024-03-08 10:28:12 +0100

    load entire csv to memory

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,4 +1,5 @@
 use crate::rest::ReferenceList;
+use rayon::prelude::*;
 use serde::{de, Deserialize};
 pub mod max_dtw;
 pub mod rest;
@@ -18,33 +19,28 @@ fn deserialize_json_string<'de, T: Deserialize<'de>, D: de::Deserializer<'de>>(
 fn main() -> Result<(), csv::Error> {
     let begin = std::time::Instant::now();
     println!("begin reading csv");
-    let rdr = csv::Reader::from_path("porto.csv");
+    let par_records = csv::Reader::from_path("porto.csv")?
+        .deserialize()
+        .map(|res| {
+            res.map(|traj: CsvTrajectory| {
+                traj.polyline
+                    .iter()
+                    .map(|&pnt| pnt.into())
+                    .collect::<Vec<_>>()
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    println!("csv in memory {:.2?}", begin.elapsed());
 
     let mut mrt_list = ReferenceList(Vec::new());
 
-    // Generate reference set
-    for (i, res) in rdr?.deserialize().enumerate() {
-        println!("progress {:.2?}%", (i as f64 * 100.0) / 1600000.0);
-        let traj: CsvTrajectory = res?;
-        let sub_start = std::time::Instant::now();
-        let t = traj
-            .polyline
-            .iter()
-            .map(|&pnt| pnt.into())
-            .collect::<Vec<_>>();
-        println!(
-            "begin encoding trajectory: {:?} with {:?} in mrt set",
-            t.len(),
-            mrt_list.0.len()
-        );
+    par_records.into_iter().for_each(|t| {
         let (_encoded_t, compression_ratio) = mrt_list.encode(&t, 0.2);
-        println!("{:?}", compression_ratio);
         if compression_ratio < 5.0 {
-            println!("inserting length: {:?} to mrt_set \n", t.len());
             mrt_list.0.push(t);
         }
-        println!("duration: {:.2?}", sub_start.elapsed());
-    }
+    });
     let elapsed = begin.elapsed();
     println!("Reference set size: {:?}", mrt_list.0.len());
     println!("duration {:.2?}", elapsed);
