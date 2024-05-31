8b313fa856ba1b18f4df30408d40d629dcca7189
Lars Skifjeld <larssski@gmail.com>
2024-05-31 14:03:10 +0200

    add performance plotting dir

--- plotting/plot.py
+++ plotting/plot.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+def no_format(value):
+    return value
 def format_1k(value):
     # Formats large numbers to 'k' notation for thousands
     return f'{int(value/1000)}k' if value >= 1000 else str(int(value))
@@ -10,7 +12,7 @@ def format_seconds_hours(value):
     hours = value / 3600  # Convert seconds to hours
     return f'{hours:.0f}h'  # Format the result as a float with two decimals followed by 'h'
 
-def plot_grouped_data_hourly_runtime(df, group_column, x_column, y_column, path, titles, x_format, y_format):
+def plot_grouped_data(df, group_column, x_column, y_column, path, titles, x_format, y_format):
     grouped_data = df.groupby(group_column)
 
     plt.figure(figsize=(10, 6))
@@ -36,10 +38,51 @@ def plot_grouped_data_hourly_runtime(df, group_column, x_column, y_column, path,
     plt.savefig(path)
 
 # Apply the function with direct conversion of runtime to hours
-data = pd.read_csv('compression.csv')
 titles = {
-    'title': 'Title',
-    'y': 'y-axis',
-    'x': 'x-axis',
+    'title': 'N over Runtime',
+    'y': 'Runtime (hours)',
+    'x': 'N',
 }
-plot_grouped_data_hourly_runtime(data, 'mode', 'n', 'runtime', 'plot.png', titles, format_1k, format_seconds_hours)
+plot_grouped_data(pd.read_csv('compression.csv'), 'mode', 'n', 'runtime', 'n_runtime.png', titles, format_1k, format_seconds_hours)
+plot_grouped_data(
+    pd.read_csv('compression.csv'),
+    'mode',
+    'n',
+    'compression_ratio',
+    'n_compression.png',
+    {
+        'title': 'N over Compression Ratio',
+        'y': 'Compression Ratio',
+        'x': 'N',
+    },
+    format_1k,
+    no_format,
+)
+plot_grouped_data(
+    pd.read_csv('set_size.csv'),
+    'mode',
+    'n',
+    'set_size',
+    'n_set_size.png',
+    {
+        'title': 'Sample Size over Set Size',
+        'y': 'Set Size',
+        'x': 'Sample Size',
+    },
+    format_1k,
+    format_1k,
+)
+plot_grouped_data(
+    pd.read_csv('set_size.csv'),
+    'mode',
+    'n',
+    'runtime',
+    'n_set_runtime.png',
+    {
+        'title': 'Set building sample size over Runtime',
+        'y': 'Runtime (hours)',
+        'x': 'Sample Size',
+    },
+    format_1k,
+    format_seconds_hours,
+)
